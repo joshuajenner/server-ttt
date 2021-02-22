27b2c1011d288e702bb35b221173a20cca8c0947
joshuajenner <joshua-jenner@hotmail.com>
2021-02-22 20:24:33 +0800

    board can be full now

--- index.js
+++ index.js
@@ -255,16 +255,15 @@ io.on("connection", (socket) => {
           }
         }
         io.to(arg[0]).emit('boardchanged', [gameRooms[b].board, gameRooms[b].turn]);
-        if (isBoardFull(gameRooms[b].board)) {
-          io.to(arg[0]).emit('boardfull');
-          console.log(arg[0]);
-          closeRoom(arg[0]);
-        }
         let winTemp = checkWin(gameRooms[b].board);
         if (winTemp[0]) {
           io.to(arg[0]).emit('winner', [arg[2], winTemp[1]]);
           closeRoom(arg[0]);
+        } else if (isBoardFull(gameRooms[b].board)) {
+          io.to(arg[0]).emit('boardfull');
+          closeRoom(arg[0]);
         }
+        
       }
     }
 
